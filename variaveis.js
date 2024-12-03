e8b314dd83cf62b9d278031b603b7bf46cff35a8
Lucas <lucasdejesus2007@gmail.com>
2024-12-03 11:03:47 -0300

    adicionado mais variaveis

--- variaveis.js
+++ variaveis.js
@@ -1,24 +1,42 @@
+var cor = "vermeio"
+let comida = "maçã"
+const juncao = "é"
+
 console.log("---- var ----");
 if (true) {
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
-
+console.log("-")
+console.log(comida, " ", juncao, " ", cor)
+cor = "roxa"
+comida = "uva"
+console.log("-")
 console.log("---- let ----");
 if (true) {
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
 // console.log(exemploLet); // Erro: exemploLet não está definido.
-
+console.log("-")
+console.log(comida, " ", juncao, " ", cor)
+cor = "amarela"
+comida = "banana"
+console.log("-")
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
 // PI = 3.15; // Erro: Não é permitido alterar uma const
-
+console.log("-")
+console.log(comida, " ", juncao, " ", cor)
+cor = "branco"
+comida = "arroz"
+console.log("-")
 function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
 }
 escopoLocal();
 // console.log(local); // Erro: 'local' é inacessível fora da função
+console.log("-")
+console.log(comida, " ", juncao, " ", cor)
