5715201b1c52acfc82211bca37a0bf0ce1da8049
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2024-12-03 11:08:23 -0300

    variaves

--- variaveis.js
+++ variaveis.js
@@ -16,9 +16,28 @@ const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
 // PI = 3.15; // Erro: Não é permitido alterar uma const
 
+console.log("---- var2 ----");
+if (true) {
+    var exemploVar2 = "Var2 fora do bloco!";
+}
+
+console.log("---- let2 ----");
+if (true) {
+    let exemploLet2 = "Let2 visível apenas dentro do bloco!";
+}
+
 function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
 }
+
+console.log("---- const2 ----");
+const dois = 2; // Valor que não muda
+console.log(dois); // 2
+
+console.log(exemploVar2); // Funciona, mas pode causar problemas.
+console.log(exemploLet2); // Funciona aqui.
+dois = 2;
+
 escopoLocal();
 // console.log(local); // Erro: 'local' é inacessível fora da função
