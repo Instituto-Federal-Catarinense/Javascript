25091faa617d6db4196fbd92ea41a1c8ca453905
pamela <pamelalentzespindula@gmail.com>
2024-12-05 09:29:42 -0300

    v1.0.1

--- variaveis.js
+++ variaveis.js
@@ -1,8 +1,14 @@
+var nome = "Pâmela"
+console.log(nome);
+
 console.log("---- var ----");
 if (true) {
+    var nome = "Emilia"
+    console.log(nome);
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
+console.log(nome);
 
 console.log("---- let ----");
 if (true) {
