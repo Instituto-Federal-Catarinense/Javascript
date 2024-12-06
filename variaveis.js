8d92ff7081c388e03b16edd825c18ee513b055b4
Ruan Borges da Silveira <borgesruan051@gmail.com>
2024-12-06 09:35:02 -0300

    v1.0.1

--- variaveis.js
+++ variaveis.js
@@ -1,15 +1,21 @@
+var nome = "Ruan";
+console.log(nome);
+
 console.log("---- var ----");
 if (true) {
+    var nome = "Chaves";
+    console.log(nome);
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
+console.log(nome);
 
 console.log("---- let ----");
 if (true) {
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
+ // console.log(exemploLet); // Erro: exemploLet não está definido.
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
