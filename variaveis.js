000172459bc4748f54e482b058f0155ff4d14162
vinislimas <vinislimas29@gmail.com>
2024-12-06 09:01:47 -0300

    teste de escopo

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,6 @@
+var nome ="vinicius" 
+console.log(nome)
+
 console.log("---- var ----");
 if (true) {
     var exemploVar = "Visível fora do bloco!";
@@ -9,19 +12,27 @@ if (true) {
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
+console.log(exemploLet); // Erro: exemploLet não está definido.
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
-// PI = 3.15; // Erro: Não é permitido alterar uma const
+PI = 3.15; // Erro: Não é permitido alterar uma const
+
+function escopoLocal() {
+    let local = "aleatorices: "; var nome =("vinicius");
+    console.log(nome);
+    
+    console.log(local);var nome = ("vinicius")
+    console.log(nome)    
+}
 
 function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
-}
+} 
 escopoLocal();
-// console.log(local); // Erro: 'local' é inacessível fora da função
+console.log(local); // Erro: 'local' é inacessível fora da função
 
 console.log("---let---")
 if(true){
