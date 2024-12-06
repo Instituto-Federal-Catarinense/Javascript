33a232f4ed22d362dbc91b67a7466f8f81ef6d53
brenda359rodrigues@gmail.com <kamilicasagrandeb@gmail.com>
2024-12-06 08:59:08 -0300

    adicionei meu nome e sobrenome

--- variaveis.js
+++ variaveis.js
@@ -1,16 +1,23 @@
+var nome = "Kamili";
+console.log (nome);
+
 console.log("---- var ----");
 if (true) {
+    console.log("Acessível dentro do bloco:" +nome)
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
 
 console.log("---- let ----");
+let sobrenome ="Casagrande";
 if (true) {
+    console.log(sobrenome); //Funciona aqui.
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
 // console.log(exemploLet); // Erro: exemploLet não está definido.
 
+console.log("Acessível globalmente:" +sobrenome);
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
@@ -20,20 +27,21 @@ function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
 }
-escopoLocal();
+
+//escopoLocal();
 // console.log(local); // Erro: 'local' é inacessível fora da função
 
 // brincando na aula 
 
-function teste() {
-    let local = "Testando123";
-    console.log(local);
-}
-teste();
+//function teste() {
+   // let local = "Testando123";
+   // console.log(local);
+//}
+//teste();
 //af
 
-console.log("teste2");
-const teste = 9090;
-console.log(teste2);
+//console.log("teste2");
+//const teste = 9090;
+//console.log(teste2);
 //teste2= 377373;
 
