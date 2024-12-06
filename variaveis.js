28e1ba07d493d880006731040aaeace5f7565302
gustavo <gustavob.ifc@gmail.com>
2024-12-06 10:47:59 -0300

    teste de escopo

--- variaveis.js
+++ variaveis.js
@@ -1,29 +1,31 @@
-var nome = "Gustavo";
+let nome = "Biny";
 console.log(nome);
 
 console.log("---- var ----");
 if (true) {
-    var nome = "bez";
-    console.log(nome);
-    var exemploVar = "Visível fora do bloco!";
+    let nome = " Binyamin";
+    console.log("Acessível dentro do bloco: " + nome);
 }
-console.log(exemploVar); // Funciona, mas pode causar problemas.
+console.log(nome); 
 
 console.log("---- let ----");
+let sobrenome = "Borges";
 if (true) {
+    console.log(sobrenome); //  funciona aqui.
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
+//console.log(exemploLet); // Erro: exemploLet não está definido.
+console.log("Acessível globalmente: " + sobrenome);
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
-//PI = 3.15; // Erro: Não é permitido alterar uma const
+// PI = 3.15; // Erro: Não é permitido alterar uma const
 
 function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
 }
 escopoLocal();
-//console.log(local); // Erro: 'local' é inacessível fora da função BEZ BABÃO
+// console.log(local); // Erro: 'local' é inacessível fora da função
