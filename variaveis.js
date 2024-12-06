e80759d4bd84a8823035d63c10dad636e86ada50
William Floripo <you@example.com>
2024-12-06 09:03:00 -0300

    Teste de escopo

--- variaveis.js
+++ variaveis.js
@@ -1,14 +1,23 @@
+var nome = "william";
+console.log(nome);
+
+
 console.log("---- var ----");
 if (true) {
+    console.log("ACessivel dentro do bloco: " + nome);
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
 
 console.log("---- let ----");
+let sobrenome = "floripo";
 if (true) {
+    console.log(sobrenome);
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
+console.log("Acessivel globalmente: " + sobrenome)
+
 console.log("Tentando acessar exemploLet fora do bloco:");
 try {
     console.log(exemploLet); // Erro: exemploLet não está definido
