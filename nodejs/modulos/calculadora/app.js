613b95c54d83f7e0401b1be1ec9b71f325f97d4b
Cauê Fortunato <cauefortunato09@gmail.com>
2025-08-26 11:05:29 -0300

    versão calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,17 @@
+const funcoesMatematicas = require('./funcoes_matematicas.js');
+
+// Exemplo de uso:
+const resultadoSoma = funcoesMatematicas.somar(5, 3);
+console.log(`Resultado da soma: ${resultadoSoma}`);
+
+const resultadoSubtracao = funcoesMatematicas.subtrair(10, 4);
+console.log(`Resultado da subtração: ${resultadoSubtracao}`);       
+
+const resultadoMultiplicacao = funcoesMatematicas.multiplicar(6, 7);
+console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
+
+const resultadoDivisao = funcoesMatematicas.dividir(20, 5);
+console.log(`Resultado da divisão: ${resultadoDivisao}`);   
+
+const idade = funcoesMatematicas.subtrair(2025,2008);
+console.log(`Resultado da subtração: ${idade}`);
