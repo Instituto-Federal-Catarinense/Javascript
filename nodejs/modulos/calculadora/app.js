001f2500c2de18faab222a035d7b2006b80d8ea8
Erick Inacio Trajano <erickinaciot@gmail.com>
2025-08-26 11:05:26 -0300

    versão calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,8 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+// Exemplo de uso:
+console.log(funcoesMatematicas.somar(2, 3));
+console.log(funcoesMatematicas.subtrair(5, 2));
+console.log(funcoesMatematicas.multiplicar(4, 6));
+console.log(funcoesMatematicas.dividir(10, 2));
+console.log(funcoesMatematicas.calcularIdade(2025, 2008));
---
+++ nodejs/modulos/calculadora/funcoes_matematicas.js
@@ -0,0 +1,30 @@
+function calcularIdade(anoAtual, anoNasc) {
+    return anoAtual - anoNasc;
+}
+
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b === 0) {
+        throw new Error('Divisão por zero não é permitida.');
+    }
+    return a / b;
+}
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    calcularIdade
+};
