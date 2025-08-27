04577c0dfc1e81219f59b1cc653de8b9f1350ca4
Daniel Reck <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-08-27 09:25:44 -0300

    Primeira versão

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -1,4 +1,7 @@
-const saudacao = require('./meuModulo'); // Importando o módulo
+const modulo = require('./meuModulo'); // Importando o módulo
 
-const mensagem = saudacao('Joédio'); // Executando a função
+const mensagem = modulo.saudacao('Joédio'); // Executando a função
+const idadeEm2025 = calculadora_idade('Joédio', 1990)
+
 console.log(mensagem);
+console.log(idadeEm2025);
