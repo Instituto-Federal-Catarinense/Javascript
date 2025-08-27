342879a3bb85404bdc9d2567119bdc69b3f95117
GregoryPozzer <pozzergregory@gmail.com>
2025-08-27 09:25:42 -0300

    versao calculadora_idade

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -3,7 +3,7 @@ const calculadora_idade = require('./calculadora_idade');
 
 
 const mensagem = modulo.saudacao('Joédio'); // Executando a função
-const idadeEm2025 = calculadora_idade.idade ('Joédio', 1990);
+const idadeEm2025 = calculadora_idade.idade ('JoédioPr', 1990);
 
 console.log(mensagem);
 console.log (idadeEm2025);
