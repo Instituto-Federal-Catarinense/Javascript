465982d10c96a12476ca8f6a07fdb29d8a1edd74
Emanoel Becker <emanoelbc01@gmail.com>
2025-08-28 10:57:45 -0300

    oii mudeu umas coisas

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,17 @@
+const funcoes = require('./funcoesmatematicas');
+
+const calculadora_idade = require ('./calculadora_idade');
+
+const resultadoSoma = funcoes.soma(5, 3);
+console.log(`Soma: ${resultadoSoma}`);
+
+const resultadoSubtracao = funcoes.subtrai(10, 4);
+console.log(`Subtração: ${resultadoSubtracao}`);
+
+const resultadoMultiplicacao = funcoes.multiplica(5, 5);
+console.log(`Multiplicação: ${resultadoMultiplicacao}`);
+
+const resultadoDivisao = funcoes.divide(169, 13);
+console.log(`Divisão: ${resultadoDivisao}`);
+
+console.log(calculadora_idade.idade("joão", 2000, 2050));
