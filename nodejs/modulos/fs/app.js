9c9f4334a0694b677b7dc99c9fc53b2602ea4855
Guilherme Scandolara Selau <guilhermesjs9@gmail.com>
2025-09-02 10:52:05 -0300

    versão com módulos comentados

--- nodejs/modulos/fs/app.js
+++ nodejs/modulos/fs/app.js
@@ -1,11 +1,16 @@
+// importando o módulo fs (file system)
 const fs = require('fs');
 
-fs.readFile('example.txt', 'utf8', (err, data) => {
+// lendo um arquivo de forma assíncrona
+fs.readFile('example.txt', 'utf8', (err, data) => { // callback
+    // tratando erros
     if (err) {
         console.error('Erro ao ler o arquivo:', err);
         return;
     }
+    // exibindo o conteúdo do arquivo no console
     console.log('Conteúdo do arquivo:', data);
 }); 
 
+// mensagem que será exibida antes da leitura do arquivo
 console.log('Esta mensagem aparece primeiro.');
--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -1,5 +1,9 @@
+// importando o módulo http
 const http = require('http');
+// criando um servidor HTTP
 http.createServer((req, res) => {
+    // definindo o cabeçalho da resposta HTTP
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    // enviando a resposta "Hello World" para o cliente
     res.end('Hello World\n');
-}).listen(8080);
+}).listen(8080); // o servidor escuta na porta 8080
