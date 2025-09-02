76d967f2b354256ee75ddf1851641bb97777aa97
Erick Inacio Trajano <erickinaciot@gmail.com>
2025-09-02 10:50:02 -0300

    versão com modulos comentados

--- nodejs/modulos/fs/app.js
+++ nodejs/modulos/fs/app.js
@@ -1,11 +1,16 @@
+// Importa o módulo 'fs' (File System) para manipulação de arquivos
 const fs = require('fs')
 
+// Lê o arquivo 'example.txt' de forma assíncrona, usando codificação 'utf8'
 fs.readFile('example.txt', 'utf8', (erro, conteudo_do_arquivo) => {
+    // Se ocorrer um erro ao ler o arquivo, exibe a mensagem de erro no console
     if (erro) {
         console.error('Error reading file:', erro)
-        return
+        return // Encerra a execução da função de callback
     }
+    // Se não houver erro, exibe o conteúdo do arquivo no console
     console.log('File contents:', conteudo_do_arquivo)
 })
 
-console.log('This is a simple Node.js application.')
+// Exibe uma mensagem no console antes da leitura do arquivo ser concluída
+console.log('Esssa mensagem aparece primeiro, antes do conteúdo do arquivo ser lido.')
