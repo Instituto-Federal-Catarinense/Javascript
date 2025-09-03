0b255e19fe1ed0d79e1d5cfbce11c1f8ace2820c
Daniel Reck <danielreckdesouza1@gmail.com>
2025-09-03 09:06:54 -0300

    Segunda versão

--- nodejs/modulos/app.js
+++
@@ -1,7 +0,0 @@
-const modulo = require('./meuModulo'); // Importando o módulo
-
-const mensagem = modulo.saudacao('Joédio'); // Executando a função
-const idadeEm2025 = calculadora_idade('Joédio', 1990)
-
-console.log(mensagem);
-console.log(idadeEm2025);
