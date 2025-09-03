ec5ec15c8b5cfb15f1f784818130c3322c1a4dd1
Agatha Scandolara Daros <Agatha.Scandolaraaa@gmail.com>
2025-09-03 09:10:38 -0300

    Versão atualizada

---
+++ nodejs/modulos/fs/app.js
@@ -0,0 +1,17 @@
+//importando o modulo fs (file system)
+const fs = require ('fs');
+
+//lendo um arquivo de forma assincrona
+fs.readFile ('exempl.txt' , 'utf8' , (erro, dados) => {
+    //tratando erro
+    if (erro) {
+        //adicionando o erro no console
+        console.error('Erro ao ler o arquivo:' , erro);
+        //Retornando para não executar o console.log abaixo
+        return;
+    }
+    //se não houver erro, exibindo o conteudo do arquivo
+    console.log ('Conteudo do arquivo' , dados);
+});
+//imprimindo uma mensagem antes da leitutra do arquivo
+console.log ('Esta mensagem aparece primeiro.');
