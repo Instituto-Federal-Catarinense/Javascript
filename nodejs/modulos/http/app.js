199777e8c86a0229cfd368cc59695df2799a090f
Apolo Miguel Hernández Vásquez <apoloh768@gmail.com>
2025-09-04 11:14:28 -0300

    Versão com o app.js e cliente.js do http

---
+++ nodejs/modulos/http/app.js
@@ -0,0 +1,11 @@
+//Importando o modulo http
+const http = require('http');
+
+//criando um servidor que responde com "Hello World!"para todas as requisições
+http.createServer((req, res) => {
+    //definindo o cabeçalho da resposta com status 200 (OK) e o tipo de conteudo text/plain
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    //enviando a resposta "Hello World!"
+    res.end('Olá, Mundo!');
+    //adicionando um log no console para cada requisição recebida
+}).listen(8080);
