5f1342270a5ab0ff4334f56d9dd141fad444e6e6
anaclara.somb@gmail.com <anaclara.somb@gmail.com>
2025-09-05 09:28:07 -0300

    VHTTP

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -6,6 +6,7 @@ http.createServer((req, res)=> {
     //definindo o cabeçalho da resposta com status 200 (ok) e tipo de conteudo text/plain
     res.writeHead(200, {'Content-type': 'text/plain'});
     //enviando a resposta "Hello word"
-    res.end('Hello world!');
+    res.end('Ola mundo!');
     //adicionando um log no console para cada requisição recebida
 }).listen(8080);
+console.log('servidor rodando em http://localhost:8080/');
