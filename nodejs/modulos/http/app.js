1eaa8a2c2557f147cc1507604e57c2e03224786a
Erick de Souza Pereira <ericksouzap56@gmail.com>
2025-09-05 09:25:02 -0300

    Versão http

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -6,6 +6,7 @@ http.createServer((req,res) => {
     //definindo o cabecalho da resposta com status 200 (OK) e tipo de conteudo texto/plain
     res.writeHead(200, {'content-type': 'text/plain'});
     //enviado a resposta "Hello World!"
-    res.end('Hello World!');
+    res.end('Ola, Mundo!');
     //adicionando um log no console para cada requisicao recebida
-}).listen(8080)
+}).listen(8080);
+console.log('Servidor rodando em http://localhost:8080/');
